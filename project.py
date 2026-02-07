aa3e427745aab605dbe1178ec9e42e2813e027de
PeterJohnson-anony <1104075299@qq.com>
2026-02-07 01:23:41 +0000

    1

--- project.py
+++ project.py
@@ -67,4 +67,5 @@ def predict_credit():
 
 if __name__ == '__main__':
     # host='0.0.0.0' 是在 Codespaces 中正常预览的关键
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
